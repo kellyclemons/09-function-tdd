8a3949ea32ab68cc3a4a5f9a6e5b9ae95d4e3d88
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-02-13 17:58:23 -0600

    did alive neighbours

--- app/arrays/array-sum.js
+++ app/arrays/array-sum.js
@@ -1,7 +1,7 @@
 export default function sum(arr) {
   let sumValue = 0;
 
-  for (let i = 0; i < arr.length; i ++) {
+  for (let i = 0; i < arr.length; i += 1) {
     sumValue = sumValue + arr[i];
   }
 
