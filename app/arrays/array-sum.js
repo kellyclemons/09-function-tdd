dfa1731a1c56e535d1a546264b6a1d5c09278b2c
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-02-13 17:08:27 -0600

    did array sum

--- app/arrays/array-sum.js
+++ app/arrays/array-sum.js
@@ -1,1 +1,9 @@
-export default function () {}
+export default function sum(arr) {
+  let sumValue = 0;
+
+  for (let i = 0; i < arr.length; i ++) {
+    sumValue = sumValue + arr[i];
+  }
+
+  return sumValue;
+}
