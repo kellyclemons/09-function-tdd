aa8e1d9d0ce2b8244e880f67d8228041957f15ef
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-02-13 18:18:06 -0600

    finished jsbs

--- app/conway/should-revive.js
+++ app/conway/should-revive.js
@@ -1,3 +1,9 @@
 import aliveNeighbors from './alive-neighbors';
 
-export default function () {}
+export default function shouldRevive(arr) {
+  if (aliveNeighbors(arr) ===  3) {
+    return true;
+  }
+
+  return false;
+}
